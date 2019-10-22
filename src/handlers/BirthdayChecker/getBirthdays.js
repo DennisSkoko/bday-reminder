643f6ab70a65808ddba3bade57f4dc4dcad5257d
Dennis Skoko <dennis.skoko@hotmail.com>
2019-10-22 21:07:01 +0200

    Changes the structure in database and improves the fetching performance

--- src/handlers/BirthdayChecker/getBirthdays.js
+++ src/handlers/BirthdayChecker/getBirthdays.js
@@ -2,24 +2,41 @@
 
 const DynamoDB = require('aws-sdk/clients/dynamodb')
 
-const client = new DynamoDB.DocumentClient({ apiVersion: '2011-12-05' })
+const client = new DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
 
 function getCurrentDate() {
-  const currentDate = new Date()
+  const date = new Date()
 
-  const year = currentDate.getFullYear()
-  const month = ('0' + (currentDate.getMonth() + 1)).slice(-2)
-  const date = ('0' + currentDate.getDate()).slice(-2)
+  const month = ('0' + (date.getMonth() + 1)).slice(-2)
+  const day = ('0' + date.getDate()).slice(-2)
 
-  return `${year}-${month}-${date}`
+  return `${month}-${day}`
+}
+
+function transformBirthdays({ BirthDate, Id, Name, BirthYear }) {
+  return {
+    birthDate: BirthDate,
+    id: Id,
+    name: Name,
+    birthYear: BirthYear
+  }
 }
 
 async function getBirthdays() {
   const res = await client
-    .scan({ TableName: process.env.TABLE_NAME_BIRTHDAY })
+    .query({
+      TableName: process.env.TABLE_NAME_BIRTHDAY,
+      KeyConditionExpression: '#BirthDate = :date',
+      ExpressionAttributeNames: {
+        '#BirthDate': 'BirthDate'
+      },
+      ExpressionAttributeValues: {
+        ':date': getCurrentDate()
+      }
+    })
     .promise()
 
-  return res.Items.filter(item => item.Birthdate === getCurrentDate())
+  return res.Items.map(transformBirthdays)
 }
 
 module.exports = getBirthdays
