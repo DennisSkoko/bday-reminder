6428a27b304beddb39673ab0643e7f13cce96d91
Dennis Skoko <dennis.skoko@hotmail.com>
2023-09-10 08:27:15 +0000

    Add support for fetching upcoming birthdays

--- src/api/src/main.js
+++ src/api/src/main.js
@@ -3,6 +3,7 @@ import helmet from 'helmet'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { v4 } from 'uuid'
+import { add, isWithinInterval, parse, isBefore } from 'date-fns'
 import * as storage from './person.js'
 
 const app = express()
@@ -32,6 +33,25 @@ app.delete('/person/:id', async (req, res) => {
   await storage.remove(req.params.id)
 
   res.sendStatus(204)
+})
+
+app.get('/birthday/upcoming', async (_req, res) => {
+  const start = new Date()
+  const end = add(start, { months: 1 })
+  const people = await storage.list()
+  const now = new Date()
+
+  const upcomingBirthdays = people.filter(person => {
+    let birthday = parse(person.birthday.slice(5), 'MM-dd', now)
+
+    if (isBefore(birthday, now)) {
+      birthday = add(birthday, { years: 1 })
+    }
+
+    return isWithinInterval(birthday, { start, end })
+  })
+
+  res.json(upcomingBirthdays)
 })
 
 app.listen(5000, () => {
